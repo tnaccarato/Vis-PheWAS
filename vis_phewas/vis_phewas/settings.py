76b121107f462995dd8fe40fedfa73dc5c6c4115
tom-naccarato <tcarlnaccarato@gmail.com>
2024-06-05 10:52:53 +0100

    Add postgres

--- vis_phewas/vis_phewas/settings.py
+++ vis_phewas/vis_phewas/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'vis_phewas.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'vis_phewas_db',
+        'USER': 'postgres',
+        'PASSWORD': 'Suss3x225!',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
