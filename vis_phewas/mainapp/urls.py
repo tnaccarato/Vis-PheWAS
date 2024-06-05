1cd63775c6c0f500f9271db4a3d4dc55aec5fde4
Tom Naccarato <108072398+tnaccarato@users.noreply.github.com>
2024-06-05 15:02:02 +0100

    Add basic tree

--- vis_phewas/mainapp/urls.py
+++ vis_phewas/mainapp/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('get_data/<str:level>/', views.mind_map_data, name='get_data_level'),
+    path('get_data/<str:level>/<str:identifier>/', views.mind_map_data, name='get_data_identifier'),
 ]
