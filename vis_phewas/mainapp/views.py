fd37c0ce5760be0107ae21422c2618fd71f86b80
Tom Naccarato <108072398+tnaccarato@users.noreply.github.com>
2024-06-05 12:53:03 +0100

    Add index template

--- vis_phewas/mainapp/views.py
+++ vis_phewas/mainapp/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import HlaPheWasCatalog
+
+
+def index(request):
+    """
+    View function for home page of site.
+    """
+    # Generate counts of some of the main objects
+    num_entries = HlaPheWasCatalog.objects.all().count()
+
+    context = {
+        'num_entries': num_entries,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'mainapp/index.html', context)
