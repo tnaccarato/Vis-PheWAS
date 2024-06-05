08d7695d1a668d9dbcc4f60765e9ecae9ecc6e53
Tom Naccarato <108072398+tnaccarato@users.noreply.github.com>
2024-06-05 11:10:20 +0100

    Add model

--- vis_phewas/mainapp/admin.py
+++ vis_phewas/mainapp/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+# Register Catalog model
+from .models import HlaPheWasCatalog
+admin.site.register(HlaPheWasCatalog)
--- vis_phewas/mainapp/models.py
+++ vis_phewas/mainapp/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
-# Create your models here.
+
+class HlaPheWasCatalog(models.Model):
+    """
+    Model representing a HLA PheWas Catalog entry.
+
+    Fields:
+    snp: The SNP identifier.
+    phewas_code: The PheWas code.
+    phewas_string: The PheWas string.
+    cases: The number of cases.
+    controls: The number of controls.
+    category_string: The disease category string.
+    odds_ratio: The odds ratio of the SNP.
+    p: The p-value.
+    l95: The lower 95% confidence interval.
+    u95: The upper 95% confidence interval.
+    gene_name: The gene name.
+    maf: The minor allele frequency.
+    a1: The first allele.
+    a2: The second allele.
+    chromosome: The chromosome number.
+    nchrobs: The number of chromosome observations.
+    """
+    snp = models.CharField(max_length=100)
+    phewas_code = models.FloatField()
+    phewas_string = models.CharField(max_length=255)
+    cases = models.IntegerField()
+    controls = models.IntegerField()
+    category_string = models.CharField(max_length=100)
+    odds_ratio = models.FloatField()
+    p = models.FloatField()
+    l95 = models.FloatField()
+    u95 = models.FloatField()
+    gene_name = models.CharField(max_length=50)
+    maf = models.FloatField()
+    a1 = models.CharField(max_length=10)
+    a2 = models.CharField(max_length=10)
+    chromosome = models.IntegerField()
+    nchrobs = models.IntegerField()
+
+    def __str__(self):
+        return self.snp
