3b62f7a40f6a0a833e3ad60d874485a7e138a124
tom-naccarato <tcarlnaccarato@gmail.com>
2024-06-06 12:53:54 +0100

    Add context processor to get field names

---
+++ vis_phewas/mainapp/context_processors.py
@@ -0,0 +1,12 @@
+from typing import Dict, List, Any
+from mainapp.models import HlaPheWasCatalog
+
+def model_fields(request) -> dict[str, list[Any]]:
+    """
+    Context processor to get the model fields for the HlaPheWasCatalog model.
+    :param request:
+    :return: Dictionary containing the model fields
+    """
+    fields = HlaPheWasCatalog._meta.get_fields()  # Retrieve all field objects from the model
+    field_names = [field.name for field in fields]  # Extract the 'name' attribute from each field object
+    return {'model_fields': field_names}  # Return a dictionary with the field names
