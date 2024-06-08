aad9789da77f7b1b8cc778c73f6d48077561001a
Tom Naccarato <108072398+tnaccarato@users.noreply.github.com>
2024-06-08 10:43:08 +0100

    Add build automation on run

--- vis_phewas/mainapp/apps.py
+++ vis_phewas/mainapp/apps.py
@@ -4,3 +4,11 @@ from django.apps import AppConfig
 class MainappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'mainapp'
+
+    def ready(self):
+        def run_build():
+            import os
+            os.system('npm run build')
+            print('Build completed')
+
+        run_build()
