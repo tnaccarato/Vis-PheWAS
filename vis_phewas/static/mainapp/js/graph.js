2a487eacbdd2774b8790b8a56b99c21834c3b781
tom-naccarato <tcarlnaccarato@gmail.com>
2024-06-18 13:30:20 +0100

    Separate main into submodules

---
+++ vis_phewas/static/mainapp/js/graph.js
@@ -0,0 +1,98 @@
+import {filters} from "./filter";
+import forceAtlas2 from "graphology-layout-forceatlas2";
+import {rgbaToFloat} from "sigma/utils";
+
+export function clickedNode(graph, node, fetchGraphData, adjustSigmaContainerHeight, getInfoTable) {
+    const nodeData = graph.getNodeAttributes(node);
+    if (nodeData.node_type === 'category') {
+        fetchGraphData({type: 'diseases', category_id: node, filters: filters, clicked: true});
+    } else if (nodeData.node_type === 'disease') {
+        fetchGraphData({type: 'alleles', disease_id: encodeURIComponent(node), filters: filters, clicked: true});
+    } else if (nodeData.node_type === 'allele') {
+        const leftColumn = document.getElementsByClassName('col-md-6 left-column')[0]
+        leftColumn.style.width = '70%';
+        // Resize the Sigma container
+        adjustSigmaContainerHeight();
+        const rightColumn = document.getElementsByClassName('col-md-6 right-column')[0]
+        rightColumn.style.width = '30%';
+        rightColumn.style.display = 'inline-block';
+        const infoPanel = document.getElementsByClassName('info-container')[0];
+        infoPanel.style.display = 'inline-block';
+        getInfoTable(nodeData);
+    }
+}
+
+export function hoverOnNode(node, graph, sigmaInstance) {
+    const nodeId = node;
+    const edges = graph.edges().filter(edge => {
+        return graph.source(edge) === nodeId || graph.target(edge) === nodeId;
+    });
+
+    console.log('Edges:', edges); // Debugging log
+
+    edges.forEach(edge => {
+        graph.setEdgeAttribute(edge, 'color', 'black');
+
+        // Sets the color of the target node to green
+        const sourceNode = graph.source(edge);
+        graph.setNodeAttribute(sourceNode, 'color', '#69fb00');
+
+        // Sets the color of the target node to a darker green
+        const targetNode = graph.target(edge);
+        graph.setNodeAttribute(targetNode, 'color', '#46af01');
+    });
+
+    sigmaInstance.refresh();
+}
+
+export function hoverOffNode(node, graph, getNodeColor, sigmaInstance) {
+    // Get the edges connected to the node
+    const nodeId = node;
+    const edges = graph.edges().filter(edge => {
+        return graph.source(edge) === nodeId || graph.target(edge) === nodeId;
+    });
+
+    edges.forEach(edge => {
+        graph.setEdgeAttribute(edge, 'color', 'darkgrey');
+        // Sets the color of the source and target nodes to their original color
+        const sourceNode = graph.source(edge);
+        const sourceNodeData = graph.getNodeAttributes(sourceNode);
+        graph.setNodeAttribute(sourceNode, 'color', getNodeColor(sourceNodeData));
+        const targetNode = graph.target(edge);
+        const targetNodeData = graph.getNodeAttributes(targetNode);
+        graph.setNodeAttribute(targetNode, 'color', getNodeColor(targetNodeData));
+    });
+
+    // Refresh the Sigma instance
+    sigmaInstance.refresh();
+}
+
+export function getApplyLayout(graph, sigmaInstance) {
+    const settings = {
+        iterations: 100,
+        settings: {
+            gravity: 0.5,
+            scalingRatio: 2.0,
+            barnesHutOptimize: true,
+            barnesHutTheta: 0.5
+        }
+    };
+    forceAtlas2.assign(graph, settings);
+    sigmaInstance.refresh();
+}
+
+export function calculateNodeColor(node) {
+    if (node.hidden) {
+        return rgbaToFloat(0, 0, 0, 0);
+    }
+    switch (node.node_type) {
+        case 'category':
+            return '#FF5733';
+        case 'disease':
+            return '#33C1FF';
+        case 'allele':
+            return '#FFFF33';
+        default:
+            return '#000000';
+    }
+}
--- vis_phewas/static/mainapp/js/main.js
+++ vis_phewas/static/mainapp/js/main.js
@@ -1,241 +1,26 @@
 import Graph from 'graphology';
 import {Sigma} from 'sigma';
-import forceAtlas2 from 'graphology-layout-forceatlas2';
-import {rgbaToFloat} from "sigma/utils";
+import {getAddFilter, getApplyFilters, getClearFilters, getRemoveFilter, getUpdateFilterInput} from "./filter";
+import {closeInfoContainer, getAdjustSigmaContainer, getExportData, getShowAlert} from "./utils";
+import {calculateNodeColor, clickedNode, getApplyLayout, hoverOffNode, hoverOnNode} from "./graph";
 
-let filterCount = 0;
-let filters = [];
-
-function push_filters() {
-    filters = [];
-    const filterGroups = document.querySelectorAll('.filter-group');
-    console.log('Filter groups:', filterGroups); // Debugging log
-
-    filterGroups.forEach(group => {
-        const select = group.querySelector('select');
-
-        const operatorSelect = group.querySelector('.operator-select');
-        const input = group.querySelector('.field-input');
-        // If input is empty, skip the filter
-        if (input.value === '') {
-            return;
-        }
-        console.log('Test')
-        console.log('Select:', select); // Debugging log
-        console.log('Operator select:', operatorSelect); // Debugging log
-        console.log('Input:', input); // Debugging log
-        if (select && input) {
-            filters.push(`${select.value}:${operatorSelect ? operatorSelect.value : '=='}:${input.value.toLowerCase()}`);
-        }
-        console.log('Filters:', filters); // Debugging log
-    });
-}
-
-function closeInfoContainer(adjustSigmaContainerHeight) {
-    return function () {
-        const leftColumn = document.getElementsByClassName('col-md-6 left-column')[0]
-        leftColumn.style.width = '100%';
-        // Resize the Sigma container
-        adjustSigmaContainerHeight();
-        const rightColumn = document.getElementsByClassName('col-md-6 right-column')[0]
-        rightColumn.style.display = 'none';
-        const infoPanel = document.getElementsByClassName('info-container')[0];
-        infoPanel.style.display = 'none';
-    };
-}
 
 // Ensure the DOM is loaded
 document.addEventListener('DOMContentLoaded', function () {
-    // Define the filter logic functions
-    window.updateFilterInput = function (select) {
-        const filterInputContainer = select.parentNode.querySelector('#filter-input-container');
-        const selectedField = select.value;
-
-        // Clear the input container
-        filterInputContainer.innerHTML = '';
-        console.log('Selected field:', selectedField); // Debugging log
-
-        // Add the appropriate input element based on the selected field
-        if (['snp', 'phewas_code', 'phewas_string', 'category_string', 'serotype', 'subtype'].includes(selectedField)) {
-            const operator = document.createElement('select');
-            operator.innerHTML = `
-            <option value="==">Exactly</option>
-            <option value="contains">Contains</option>
-            `;
-            operator.className = 'operator-select';
-            filterInputContainer.appendChild(operator);
-            const input = document.createElement('input');
-            input.type = 'text';
-            input.placeholder = 'Enter value';
-            input.className = 'field-input';
-            filterInputContainer.appendChild(input);
-
-        } else if (['cases', 'controls', 'p', 'odds_ratio', 'l95', 'u95', 'maf'].includes(selectedField)) {
-            const operator = document.createElement('select');
-            operator.innerHTML = `
-            <option value="==">== (Equal to)</option>
-            <option value=">">\> (Greater than)</option>
-            <option value="<">\< (Less than)</option>
-            <option value=">=">>\=(Greater than or equal to)</option>
-            <option value="<="><\=(Less than or equal to)</option>
-        `;
-            operator.className = 'operator-select';
-            filterInputContainer.appendChild(operator);
-            const input = document.createElement('input');
-            input.type = 'float';
-            input.placeholder = 'Enter value';
-            input.className = 'field-input';
-            filterInputContainer.appendChild(input);
-        } else if (['gene_class', 'gene_name', 'a1', 'a2'].includes(selectedField)) {
-            const select = document.createElement('select');
-            select.className = 'field-input';
-            if (selectedField === 'gene_class') {
-                select.innerHTML = `
-            <option value="1">Class 1</option>
-            <option value="2">Class 2</option>
-            `;
-            } else if (selectedField === 'gene_name') {
-                select.innerHTML = `
-            <option value="A">A</option>
-            <option value="B">B</option>
-            <option value="C">C</option>
-            <option value="DPA1">DPA1</option>
-            <option value="DPB1">DPB1</option>
-            <option value="DQA1">DQA1</option>
-            <option value="DQB1">DQB1</option>
-            <option value="DRB1">DRB1</option>
-            `;
-            } else {
-                select.innerHTML = `
-                <option value="A">A</option>
-                <option value="P">P</option>
-                `;
-            }
-            filterInputContainer.appendChild(select);
-
-        }
-
-
-        adjustSigmaContainerHeight();
-    };
-
-    window.addFilter = function () {
-        console.log('Filter count:', filterCount); // Debugging log
-        // Show the toolbar if it is hidden
-        const toolbar = document.getElementsByClassName('toolbar')[0];
-        toolbar.style.display = 'block';
-        if (filterCount >= 8) {
-            alert('Maximum of 8 filters allowed');
-        } else {
-            const filterGroup = document.createElement('div');
-            filterGroup.className = 'filter-group';
-
-            const select = document.createElement('select');
-            select.className = 'field-select';
-            select.onchange = function () {
-                updateFilterInput(select);
-            };
-            select.innerHTML = `
-            <option value="snp">SNP</option>
-            <option value="gene_class">Gene Class</option>
-            <option value="gene_name">HLA Type</option>
-            <option value="serotype">Serotype</option>
-            <option value="subtype">Subtype</option>
-            <option value="phewas_code">Phecode</option>
-            <option value="phewas_string">Phenotype</option>
-            <option value="category_string">Disease Category</option>
-            <option value="cases">Number of Cases</option>
-            <option value="controls">Number of Controls</option>
-            <option value="p">P-Value</option>
-            <option value="odds_ratio">Odds Ratio</option>
-            <option value="l95">95% CI Lower Bound</option>
-            <option value="u95">95% CI Upper Bound</option>
-            <option value="maf">Minor Allele Frequency</option>
-            <option value="a1">Allele 1</option>
-            <option value="a2">Allele 2</option>
-        `;
-            filterGroup.appendChild(select);
-
-            const filterInputContainer = document.createElement('div');
-            filterInputContainer.id = 'filter-input-container';
-            filterGroup.appendChild(filterInputContainer);
-
-            const minusButton = document.createElement('button');
-            minusButton.className = 'btn btn-danger';
-            minusButton.textContent = '-';
-            minusButton.onclick = function () {
-                removeFilter(minusButton);
-            };
-            filterGroup.appendChild(minusButton);
-
-            document.getElementById('filters-container').appendChild(filterGroup);
-
-            // Gets content of the filter group
-            window.updateFilterInput(select);
-
-            // Adjust the width of the Sigma container
-            adjustSigmaContainerHeight();
-
-            filterCount++;
-            console.log(filterCount);
-        }
-    };
-
-
-// Function to remove a filter
-    window.removeFilter = function (button) {
-        const filterGroup = button.parentNode;
-        filterGroup.remove();
-
-        adjustSigmaContainerHeight();
-        filterCount--;
-    }
-
-
-// Function to adjust the width of the Sigma container
-    function adjustSigmaContainerHeight() {
-        const filtersContainer = document.getElementById('filters-container');
-        const filtersHeight = filtersContainer.offsetHeight;
-
-        container.style.height = `calc(100% - ${filtersHeight}px)`;
-        sigmaInstance.refresh();
-    }
-
-    window.applyFilters = function () {
-        push_filters();
-        // Display a dismissible alert message for confirmation of filters applied
-        const message = filters.length > 0 ? `Applying filters: ${filters.join(', ')}` : 'No filters applied';
-        showAlert(message);
-
-        console.log('Filters:', filters); // Debugging log
-
-        fetchGraphData({type: 'initial', filters});
-
-        sigmaInstance.refresh();
-
-    };
-
-
-    window.clearFilters = function () {
-        filters = [];
-        const filterGroups = document.querySelectorAll('.filter-group');
-        filterGroups.forEach(group => {
-            group.remove();
-        });
-        filterCount = 0;
-        // Clear the info container
-        closeInfoContainer(adjustSigmaContainerHeight)();
-        // Hide the toolbar
-        const toolbar = document.getElementsByClassName('toolbar')[0];
-        toolbar.style.display = 'none';
-        // Display an alert message for confirmation of filters cleared
-        showAlert('Filters cleared. Showing all data.');
-        fetchGraphData();
-    };
-
     const container = document.getElementById('sigma-container');
     const graph = new Graph({multi: true});
-    const sigmaInstance = new Sigma(graph, container, {allowInvalidContainer: true})
+    const sigmaInstance = new Sigma(graph, container, {allowInvalidContainer: true});
+    window.updateFilterInput = getUpdateFilterInput(adjustSigmaContainerHeight);
+    window.addFilter = getAddFilter(adjustSigmaContainerHeight);
+    window.removeFilter = getRemoveFilter(adjustSigmaContainerHeight)
+
+    function adjustSigmaContainerHeight() {
+        getAdjustSigmaContainer(container, sigmaInstance);
+    }
+
+    window.applyFilters = getApplyFilters(showAlert, fetchGraphData, sigmaInstance);
+    window.clearFilters = getClearFilters(adjustSigmaContainerHeight, showAlert, fetchGraphData);
+
 
 
     fetchGraphData()
@@ -354,33 +139,11 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     function getNodeColor(node) {
-        if (node.hidden) {
-            return rgbaToFloat(0, 0, 0, 0);
-        }
-        switch (node.node_type) {
-            case 'category':
-                return '#FF5733';
-            case 'disease':
-                return '#33C1FF';
-            case 'allele':
-                return '#FFFF33';
-            default:
-                return '#000000';
-        }
+        return calculateNodeColor(node);
     }
 
     function applyLayout() {
-        const settings = {
-            iterations: 100,
-            settings: {
-                gravity: 0.5,
-                scalingRatio: 2.0,
-                barnesHutOptimize: true,
-                barnesHutTheta: 0.5
-            }
-        };
-        forceAtlas2.assign(graph, settings);
-        sigmaInstance.refresh();
+        getApplyLayout(graph, sigmaInstance);
     }
 
     function getInfoTable(nodeData) {
@@ -509,139 +272,28 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     sigmaInstance.on('clickNode', ({node}) => {
-            const nodeData = graph.getNodeAttributes(node);
-            if (nodeData.node_type === 'category') {
-                fetchGraphData({type: 'diseases', category_id: node, filters: filters, clicked: true});
-            } else if (nodeData.node_type === 'disease') {
-                fetchGraphData({type: 'alleles', disease_id: encodeURIComponent(node), filters: filters, clicked: true});
-            } else if (nodeData.node_type === 'allele') {
-                const leftColumn = document.getElementsByClassName('col-md-6 left-column')[0]
-                leftColumn.style.width = '70%';
-                // Resize the Sigma container
-                adjustSigmaContainerHeight();
-                const rightColumn = document.getElementsByClassName('col-md-6 right-column')[0]
-                rightColumn.style.width = '30%';
-                rightColumn.style.display = 'inline-block';
-                const infoPanel = document.getElementsByClassName('info-container')[0];
-                infoPanel.style.display = 'inline-block';
-                getInfoTable(nodeData);
-            }
+            clickedNode(graph, node, fetchGraphData, adjustSigmaContainerHeight, getInfoTable);
         }
     );
 
     // Event listener for when a node hover event is triggered
     sigmaInstance.on('enterNode', ({node}) => {
-        const nodeId = node;
-        const edges = graph.edges().filter(edge => {
-            return graph.source(edge) === nodeId || graph.target(edge) === nodeId;
-        });
-
-        console.log('Edges:', edges); // Debugging log
-
-        edges.forEach(edge => {
-            graph.setEdgeAttribute(edge, 'color', 'black');
-
-            // Sets the color of the target node to green
-            const sourceNode = graph.source(edge);
-            graph.setNodeAttribute(sourceNode, 'color', '#69fb00');
-
-            // Sets the color of the target node to a darker green
-            const targetNode = graph.target(edge);
-            graph.setNodeAttribute(targetNode, 'color', '#46af01');
-        });
-
-        sigmaInstance.refresh();
+        hoverOnNode(node, graph, sigmaInstance);
     });
 
 // Event listener for when a node hover ends
     sigmaInstance.on('leaveNode', ({node}) => {
-        // Get the edges connected to the node
-        const nodeId = node;
-        const edges = graph.edges().filter(edge => {
-            return graph.source(edge) === nodeId || graph.target(edge) === nodeId;
-        });
-
-        edges.forEach(edge => {
-            graph.setEdgeAttribute(edge, 'color', 'darkgrey');
-            // Sets the color of the source and target nodes to their original color
-            const sourceNode = graph.source(edge);
-            const sourceNodeData = graph.getNodeAttributes(sourceNode);
-            graph.setNodeAttribute(sourceNode, 'color', getNodeColor(sourceNodeData));
-            const targetNode = graph.target(edge);
-            const targetNodeData = graph.getNodeAttributes(targetNode);
-            graph.setNodeAttribute(targetNode, 'color', getNodeColor(targetNodeData));
-        });
-
-        // Refresh the Sigma instance
-        sigmaInstance.refresh();
+        hoverOffNode(node, graph, getNodeColor, sigmaInstance);
     });
-
-    // Function to sanitize the filters for the export query
-    function sanitizeFilter(filter) {
-        return filter.replace(/&/g, 'and').replace(/</g, 'lt').replace(/>/g, 'gt')
-    }
 
     // Exports the current query to a CSV file
     window.exportQuery = function () {
-        // Push filters to the filters array to make sure the filters are applied
-        push_filters();
-        console.log('Filters:', filters); // Debugging log
-
-        // If filters is empty, set it to an empty array
-        if (!filters) {
-            filters = [];
-        }
-
-        // Construct the query string
-        const query = new URLSearchParams({filters: filters}).toString();
-        // Construct the URL from which to fetch the data
-        const url = '/api/export-query/' + (query ? '?' + query : '');
-
-        // Fetch the data from the URL
-        fetch(url)
-            // Get the response as a blob
-            .then(response => {
-                console.log(url)
-                // Get the dataset length from the response headers
-                const length = response.headers.get('Dataset-Length');
-                if (length === '0') {
-                    showAlert('No data to export');
-                    return;
-                }
-                // Construct the alert message
-                const filtersDisplay = filters.length > 0 ? filters.join(', ') : 'None';
-                const alertMessage = `Exporting Data...<br><b>Filters selected:</b> ${filtersDisplay}<br><b>Dataset length:</b> ${length}`;
-                showAlert(alertMessage);
-                // Return the response as a blob object
-                return response.blob();
-            })
-            // Convert the blob to a URL and download the file
-            .then(blob => {
-                const downloadUrl = window.URL.createObjectURL(blob);
-                const a = document.createElement('a');
-                a.href = downloadUrl;
-                a.download = 'exported_data.csv';
-                document.body.appendChild(a);
-                a.click();
-                a.remove();
-            })
-            // Log any errors to the console
-            .catch(error => console.error('Error:', error));
+        getExportData(showAlert);
     };
 
     // Function to show an alert message
     function showAlert(message) {
-        // Get the alert container
-        const alertContainer = document.getElementById('alert-container');
-        // Set the inner HTML of the alert container
-        alertContainer.innerHTML = `
-        <div class="alert alert-info alert-dismissible fade show" role="alert" style="margin: 0">
-            ${message}
-            <button type="button" class="btn-close align-center" data-bs-dismiss="alert" aria-label="Close"></button>
-        </div>
-    `;
+        getShowAlert(message);
     }
-
-
 });
 
