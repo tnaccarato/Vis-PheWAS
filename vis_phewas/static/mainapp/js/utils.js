c21619ee0b0c90fbd9506823bf234c9c09922500
tom-naccarato <tcarlnaccarato@gmail.com>
2024-07-23 15:23:03 +0100

    Reformat with prettier

--- vis_phewas/static/mainapp/js/utils.js
+++ vis_phewas/static/mainapp/js/utils.js
@@ -1,72 +1,76 @@
-import {filters, push_filters} from "./filter";
-import {scaleLog} from "d3-scale";
+import { filters, push_filters } from "./filter";
+import { scaleLog } from "d3-scale";
 import * as d3 from "d3";
 
 export function closeInfoContainer(adjustSigmaContainerHeight) {
-    return function () {
-        const leftColumn = document.getElementsByClassName('col-md-6 left-column')[0]
-        leftColumn.style.width = '100%';
-        // Resize the Sigma container
-        adjustSigmaContainerHeight();
-        const rightColumn = document.getElementsByClassName('col-md-6 right-column')[0]
-        rightColumn.style.display = 'none';
-        const infoPanel = document.getElementsByClassName('info-container')[0];
-        infoPanel.style.display = 'none';
-    };
+  return function () {
+    const leftColumn = document.getElementsByClassName(
+      "col-md-6 left-column",
+    )[0];
+    leftColumn.style.width = "100%";
+    // Resize the Sigma container
+    adjustSigmaContainerHeight();
+    const rightColumn = document.getElementsByClassName(
+      "col-md-6 right-column",
+    )[0];
+    rightColumn.style.display = "none";
+    const infoPanel = document.getElementsByClassName("info-container")[0];
+    infoPanel.style.display = "none";
+  };
 }
 
 export function getExportData(showAlert) {
-    // Push filters to the filters array to make sure the filters are applied
-    push_filters();
-    console.log('Filters:', filters); // Debugging log
+  // Push filters to the filters array to make sure the filters are applied
+  push_filters();
+  console.log("Filters:", filters); // Debugging log
 
-    // If filters is empty, set it to an empty array
-    if (!filters) {
-        filters = [];
-    }
+  // If filters is empty, set it to an empty array
+  if (!filters) {
+    filters = [];
+  }
 
-    // Construct the query string
-    const query = new URLSearchParams({filters: filters}).toString();
-    // Construct the URL from which to fetch the data
-    const url = '/api/export-query/' + (query ? '?' + query : '');
+  // Construct the query string
+  const query = new URLSearchParams({ filters: filters }).toString();
+  // Construct the URL from which to fetch the data
+  const url = "/api/export-query/" + (query ? "?" + query : "");
 
-    // Fetch the data from the URL
-    fetch(url)
-        // Get the response as a blob
-        .then(response => {
-            console.log(url)
-            // Get the dataset length from the response headers
-            const length = response.headers.get('Dataset-Length');
-            if (length === '0') {
-                showAlert('No data to export');
-                return;
-            }
-            // Construct the alert message
-            const filtersDisplay = filters.length > 0 ? filters.join(', ') : 'None';
-            const alertMessage = `Exporting Data...<br><b>Filters selected:</b> ${filtersDisplay}<br><b>Dataset length:</b> ${length}`;
-            showAlert(alertMessage);
-            // Return the response as a blob object
-            return response.blob();
-        })
-        // Convert the blob to a URL and download the file
-        .then(blob => {
-            const downloadUrl = window.URL.createObjectURL(blob);
-            const a = document.createElement('a');
-            a.href = downloadUrl;
-            a.download = 'exported_data.csv';
-            document.body.appendChild(a);
-            a.click();
-            a.remove();
-        })
-        // Log any errors to the console
-        .catch(error => console.error('Error:', error));
+  // Fetch the data from the URL
+  fetch(url)
+    // Get the response as a blob
+    .then((response) => {
+      console.log(url);
+      // Get the dataset length from the response headers
+      const length = response.headers.get("Dataset-Length");
+      if (length === "0") {
+        showAlert("No data to export");
+        return;
+      }
+      // Construct the alert message
+      const filtersDisplay = filters.length > 0 ? filters.join(", ") : "None";
+      const alertMessage = `Exporting Data...<br><b>Filters selected:</b> ${filtersDisplay}<br><b>Dataset length:</b> ${length}`;
+      showAlert(alertMessage);
+      // Return the response as a blob object
+      return response.blob();
+    })
+    // Convert the blob to a URL and download the file
+    .then((blob) => {
+      const downloadUrl = window.URL.createObjectURL(blob);
+      const a = document.createElement("a");
+      a.href = downloadUrl;
+      a.download = "exported_data.csv";
+      document.body.appendChild(a);
+      a.click();
+      a.remove();
+    })
+    // Log any errors to the console
+    .catch((error) => console.error("Error:", error));
 }
 
 export function getShowAlert(message) {
-    // Get the alert container
-    const alertContainer = document.getElementById('alert-container');
-    // Set the inner HTML of the alert container
-    alertContainer.innerHTML = `
+  // Get the alert container
+  const alertContainer = document.getElementById("alert-container");
+  // Set the inner HTML of the alert container
+  alertContainer.innerHTML = `
         <div class="alert alert-info alert-dismissible fade show" role="alert" style="margin: 0">
             ${message}
             <button type="button" class="btn-close align-center" data-bs-dismiss="alert" aria-label="Close"></button>
@@ -75,33 +79,31 @@ export function getShowAlert(message) {
 }
 
 export function getAdjustSigmaContainer(container, sigmaInstance) {
-    const filtersContainer = document.getElementById('filters-container');
-    const filtersHeight = filtersContainer.offsetHeight;
+  const filtersContainer = document.getElementById("filters-container");
+  const filtersHeight = filtersContainer.offsetHeight;
 
-    container.style.height = `calc(100% - ${filtersHeight}px)`;
-    sigmaInstance.refresh();
+  container.style.height = `calc(100% - ${filtersHeight}px)`;
+  sigmaInstance.refresh();
 }
 
 // Create a scale for the size of the nodes
-export const sizeScale = scaleLog()
-    .domain([0.00001, 0.05])
-    .range([8, 2]);
+export const sizeScale = scaleLog().domain([0.00001, 0.05]).range([8, 2]);
 
 // Create a scale for the color of the nodes
 export const protectiveColorScale = scaleLog()
-    .domain([0.01, 0.99])
-    .range([d3.rgb('#08306b'), d3.rgb('#01c6ff')]) // Range from dark blue to light blue
+  .domain([0.01, 0.99])
+  .range([d3.rgb("#08306b"), d3.rgb("#01c6ff")]); // Range from dark blue to light blue
 
 export const riskColorScale = scaleLog()
-    .domain([1, 5])
-    .range([d3.rgb('#ff7d7d'), d3.rgb('#8a040a')]);  // Range from light red to dark red
+  .domain([1, 5])
+  .range([d3.rgb("#ff7d7d"), d3.rgb("#8a040a")]); // Range from light red to dark red
 
 // Sets the color of the disease nodes based on how many alleles are associated with the disease
 export const diseaseColor = scaleLog()
-    .domain([1, 38])
-    .range([d3.rgb('#eafa05'), d3.rgb('#fa6011')]); // Range from yellow to orange
+  .domain([1, 38])
+  .range([d3.rgb("#eafa05"), d3.rgb("#fa6011")]); // Range from yellow to orange
 
 // Function to clamp values within the domain range for the scale to avoid outlying values
 export function clamp(value, domain) {
-    return Math.max(domain[0], Math.min(domain[domain.length - 1], value));
+  return Math.max(domain[0], Math.min(domain[domain.length - 1], value));
 }
