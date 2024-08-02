ff40374ddf8d1b70ea0eb37b18b3d9ceabe53671
tom-naccarato <tcarlnaccarato@gmail.com>
2024-08-02 18:23:57 +0100

    Fix slow adjust sigma container

--- vis_phewas/static/mainapp/js/FilterManager.js
+++ vis_phewas/static/mainapp/js/FilterManager.js
@@ -170,6 +170,7 @@ export class FilterManager {
     const chevron = document.querySelector(".toggle-button .fa-chevron-down");
 
     const isFilterBodyVisible = filterBody.style.display === "block";
+    this.adjustSigmaContainerHeight();
 
     filterBody.style.display = isFilterBodyVisible ? "none" : "block";
     filterContainer.style.display = isFilterBodyVisible ? "none" : "block";
@@ -184,6 +185,7 @@ export class FilterManager {
     filterContainer.style.display = "block";
     const filterBody = document.querySelector(".toolbar");
     filterBody.style.display = "block";
+    this.adjustSigmaContainerHeight();
     if (this.filterCount >= 8) {
       alert("Maximum of 8 filters allowed");
     } else {
