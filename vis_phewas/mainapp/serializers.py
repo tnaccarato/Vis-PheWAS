f60b43e0ba646a766d44c4f05c20a96beda9329c
Tom Naccarato <108072398+tnaccarato@users.noreply.github.com>
2024-08-02 11:42:09 +0100

    Convert database handling to api calls

---
+++ vis_phewas/mainapp/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import HlaPheWasCatalog
+
+
+class HlaPheWasCatalogSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = HlaPheWasCatalog
+        fields = '__all__'
