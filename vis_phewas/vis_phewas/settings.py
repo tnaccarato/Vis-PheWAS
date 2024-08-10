f1c556161b808ab58b3d8ab9347ebe278975231a
Tom Naccarato <108072398+tnaccarato@users.noreply.github.com>
2024-08-10 20:49:41 +0100

    Fix deployment
    Fixed deployment to use Whitenoise rather than nginx to simplify deployment, added separate dockerfiles for custom postgres and django application with gunicorn

--- vis_phewas/vis_phewas/settings.py
+++ vis_phewas/vis_phewas/settings.py
@@ -42,10 +42,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     "mainapp.apps.MainappConfig",
     "rest_framework",
+    'whitenoise.runserver_nostatic',  # Disable Django's static file handling in favor of WhiteNoise
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # WhiteNoise Middleware for serving static files
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -118,13 +120,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 
-STATIC_ROOT = BASE_DIR / 'staticfiles' 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# WhiteNoise configuration for compressing static files
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
