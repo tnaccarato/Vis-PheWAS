74b96968676060d844e729876a319c7474b4ac80
Tom Naccarato <108072398+tnaccarato@users.noreply.github.com>
2024-08-13 08:19:57 +0100

    Update urls.py

--- vis_phewas/som/urls.py
+++ vis_phewas/som/urls.py
@@ -5,5 +5,4 @@ from .views import SOMDiseaseView, SOMSNPView
 urlpatterns = [
     path('SOMSNP/', SOMSNPView.as_view(), name='SOM'),
     path('SOMDisease/', SOMDiseaseView.as_view(), name='SOM'),
-    path('som_button/', views.som_button, name='som_button'),
     ]
