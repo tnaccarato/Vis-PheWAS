6d6980682bf225c4dc6c5a7d6ad71ce6bfd5b757
Tom Naccarato <108072398+tnaccarato@users.noreply.github.com>
2024-08-17 19:33:26 +0100

    Refactor showSubtypes to session storage for better state management

--- vis_phewas/static/mainapp/js/UIManager.js
+++ vis_phewas/static/mainapp/js/UIManager.js
@@ -11,27 +11,31 @@ class UIManager {
   constructor() {
     this.graphManager = new GraphManager(
       "sigma-container",
-      this.adjustSigmaContainerHeight.bind(this)
+      this.adjustSigmaContainerHeight.bind(this),
     );
     this.filterManager = new FilterManager(
       this.adjustSigmaContainerHeight.bind(this),
       this.showAlert.bind(this),
       this.graphManager.fetchGraphData.bind(this.graphManager),
       this.graphManager.sigmaInstance,
-      this.graphManager
+      this.graphManager,
     );
 
     // Set the filterManager on the window object
     window.filterManager = this.filterManager;
 
     this.initEventListeners();
+    this.initShowSubtypes();
   }
 
   /**
    * Initializes event listeners for the UIManager.
    */
   initEventListeners() {
-    document.addEventListener("DOMContentLoaded", this.onDOMContentLoaded.bind(this));
+    document.addEventListener(
+      "DOMContentLoaded",
+      this.onDOMContentLoaded.bind(this),
+    );
     window.updateFilterInput = this.filterManager.updateFilterInput;
     window.addFilter = this.filterManager.addFilter;
     window.removeFilter = this.filterManager.removeFilter;
@@ -40,6 +44,7 @@ class UIManager {
     window.fetchAndShowAssociations = fetchAndShowAssociations;
     window.updateShowSubtypes = this.updateShowSubtypes.bind(this);
     window.exportQuery = this.exportQuery.bind(this);
+    window.updateCheckboxState = this.updateCheckboxState.bind(this);
   }
 
   /**
@@ -54,7 +59,10 @@ class UIManager {
    * Adjusts the height of the Sigma container.
    */
   adjustSigmaContainerHeight() {
-    getAdjustSigmaContainer(document.getElementById("sigma-container"), this.graphManager.sigmaInstance);
+    getAdjustSigmaContainer(
+      document.getElementById("sigma-container"),
+      this.graphManager.sigmaInstance,
+    );
   }
 
   /**
@@ -69,10 +77,29 @@ class UIManager {
    * Toggles the display of subtypes and refreshes the graph data.
    */
   updateShowSubtypes() {
-    window.showSubtypes = !window.showSubtypes;
-    console.log("Show subtypes:", window.showSubtypes); // Debugging log
-    this.graphManager.fetchGraphData();
-    this.graphManager.sigmaInstance.refresh();
+  console.log("showSubtypes before: " + localStorage.getItem("showSubtypes"));
+  const showSubtypes = !(localStorage.getItem("showSubtypes") === "true");
+  localStorage.setItem("showSubtypes", showSubtypes.toString());
+  this.updateCheckboxState();
+  console.log("showSubtypes updated to: " + localStorage.getItem("showSubtypes"));
+  this.graphManager.fetchGraphData();
+  this.graphManager.sigmaInstance.refresh();
+}
+
+  updateCheckboxState() {
+  const checkbox = document.getElementById('show-subtypes-switch');
+  checkbox.checked = localStorage.getItem("showSubtypes") === "true";
+}
+
+
+
+  initShowSubtypes() {
+    let storedState = localStorage.getItem("showSubtypes");
+    if (storedState === null) {
+      localStorage.setItem("showSubtypes", "false");
+    }
+
+    this.updateCheckboxState();
   }
 
   /**
