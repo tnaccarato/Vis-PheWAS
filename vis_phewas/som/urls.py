5bef6388bf4ed1a4564fc89f7769fb23d6977864
Tom Naccarato <108072398+tnaccarato@users.noreply.github.com>
2024-08-18 12:59:44 +0100

    Add rotating logs

--- vis_phewas/som/urls.py
+++ vis_phewas/som/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import SOMDiseaseView, SOMSNPView
+from .views import SOMDiseaseView, SOMSNPView, CircosDiagramView
 
 urlpatterns = [
     path('SOMSNP/', SOMSNPView.as_view(), name='SOM'),
     path('SOMDisease/', SOMDiseaseView.as_view(), name='SOM'),
+    path('circos-plot/<str:file_name>', CircosDiagramView.as_view(), name='circos-plot'),
 ]
