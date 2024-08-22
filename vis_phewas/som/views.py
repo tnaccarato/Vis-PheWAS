64e71ee0fcc1cb181cee3f45d84432b9f4a0a532
Tom Naccarato <108072398+tnaccarato@users.noreply.github.com>
2024-08-22 02:36:49 +0100

    Refactor to reduce complexity

--- vis_phewas/som/views.py
+++ vis_phewas/som/views.py
@@ -52,101 +52,12 @@ class SOMView(APIView):
         temp_data = get_object_or_404(TemporaryCSVData, id=data_id)
         filtered_df = preprocess_temp_data(temp_data)
 
-        # Group the data based on the SOM type
+        # Group the data based on the SOM type and engineer the features
         if som_type == 'snp':
-            # Group the data by SNP and aggregate the values
-            grouped_df = filtered_df.groupby('snp').agg({
-                'phewas_string': list,
-                'p': list,
-                'odds_ratio': list,
-                'category_string': list,
-                'l95': list,
-                'u95': list,
-                'maf': list,
-            }).reset_index()
-
-            # Feature engineering for SNPs
-            all_phenotypes = set([phenotype for phenotypes in grouped_df['phewas_string'] for phenotype in phenotypes])
-            exploded_df = grouped_df.explode('phewas_string').explode('category_string')
-
-            # One-Hot Encode categorical variables with sparse one-hot encoding to save memory
-            ohe_phenotype = OneHotEncoder(sparse_output=False)
-            ohe_category = OneHotEncoder(sparse_output=False)
-            # Encode the phenotype and category variables
-            phenotype_encoded = ohe_phenotype.fit_transform(exploded_df[['phewas_string']])
-            category_encoded = ohe_category.fit_transform(exploded_df[['category_string']])
-
-            # Aggregate the encoded features
-            phenotype_aggregated = pd.DataFrame(
-                phenotype_encoded,
-                columns=ohe_phenotype.categories_[0],
-                index=exploded_df['snp']
-            ).groupby('snp').sum()
-
-            # Aggregate the encoded features
-            category_aggregated = pd.DataFrame(
-                category_encoded,
-                columns=ohe_category.categories_[0],
-                index=exploded_df['snp']
-            ).groupby('snp').sum()
-
-            # Combine the aggregated features
-            encoded_features = np.hstack([phenotype_aggregated, category_aggregated])
-
-            # Create the phenotype features for each row
-            def create_phenotype_features(df_row):
-                # Create a dictionary to store the features
-                features = defaultdict(float)
-                # Iterate over the phenotypes, p-values, and odds ratios
-                for phenotype, p, or_value in zip(df_row['phewas_string'], df_row['p'], df_row['odds_ratio']):
-                    features[phenotype] = or_value * 5  # Scale the odds ratio by 5 for better visualisation
-                # Create the phenotype features
-                phenotype_features = [features[phenotype] for phenotype in all_phenotypes]
-                # Return the combined features
-                return np.hstack([phenotype_features, encoded_features[df_row.name]])
-
-            # Apply the function to create the phenotype features
-            features_matrix = grouped_df.apply(create_phenotype_features, axis=1)
+            features_matrix, grouped_df = self.engineer_snp_features(filtered_df)
 
         else:
-            # Feature engineering for diseases
-            grouped_df = filtered_df.groupby('phewas_string').agg({
-                'snp': list,
-                'gene_name': list,
-                'p': list,
-                'odds_ratio': list,
-                'category_string': 'first',
-                'l95': list,
-                'u95': list,
-                'maf': list,
-            }).reset_index()
-
-            # One-Hot Encode categorical variables with sparse one-hot encoding to save memory
-            all_alleles = sorted(set(allele for alleles in grouped_df['snp'] for allele in alleles))
-            ohe = OneHotEncoder(sparse_output=False)
-            # Encode the gene name variable with one-hot encoding
-            gene_name_encoded = ohe.fit_transform(
-                grouped_df['gene_name'].apply(lambda x: ','.join(set(x))).str.get_dummies(sep=',')
-            )
-            # Encode the category variable
-            category_encoded = ohe.fit_transform(grouped_df[['category_string']])
-            # Combine the encoded features
-            encoded_features = np.hstack([gene_name_encoded, category_encoded])
-
-            # Create the allele features for each row
-            def create_allele_features(df_row):
-                # Create a dictionary to store the features
-                features = defaultdict(float)
-                # Iterate over the alleles, p-values, and odds ratios
-                for allele, p, or_value in zip(df_row['snp'], df_row['p'], df_row['odds_ratio']):
-                    features[allele] = or_value * 5  # Scale the odds ratio by 5 for better visualisation
-                # Create the allele features
-                allele_features = [features[allele] for allele in all_alleles]
-                # Return the combined features
-                return np.hstack([allele_features, encoded_features[df_row.name]])
-
-            # Apply the function to create the allele features
-            features_matrix = grouped_df.apply(create_allele_features, axis=1)
+            features_matrix, grouped_df = self.engineer_disease_features(filtered_df)
 
         # Normalise the features matrix
         x = np.array(features_matrix.tolist())
@@ -161,24 +72,7 @@ class SOMView(APIView):
         positions_df = pd.DataFrame(positions, columns=['x', 'y'])
 
         # Create the results DataFrame based on the SOM type
-        if som_type == 'snp':
-            results_df = pd.DataFrame({
-                'snp': grouped_df['snp'],
-                'x': positions_df['x'],
-                'y': positions_df['y'],
-                'p_values': grouped_df['p'],
-                'odds_ratios': grouped_df['odds_ratio'],
-                'phenotypes': grouped_df['phewas_string']
-            })
-        else:
-            results_df = pd.DataFrame({
-                'phewas_string': grouped_df['phewas_string'],
-                'x': positions_df['x'],
-                'y': positions_df['y'],
-                'p_values': grouped_df['p'],
-                'odds_ratios': grouped_df['odds_ratio'],
-                'snps': grouped_df['snp']
-            })
+        results_df = self.construct_results_df(grouped_df, positions_df, som_type)
 
         # K-Means clustering
         kmeans = KMeans(n_clusters=int(num_clusters), random_state=42)
@@ -255,4 +149,143 @@ class SOMView(APIView):
             'filters': filters if filters else categories,
             'cleaned_filters': cleaned_filters
         }
+
+    def construct_results_df(self, grouped_df, positions_df, som_type):
+        """
+        Helper method to construct the results DataFrame based on the SOM type.
+        :param grouped_df: The grouped DataFrame
+        :param positions_df: The positions DataFrame
+        :param som_type: Type of the SOM (SNP or disease)
+        :return:
+        """
+        if som_type == 'snp':
+            results_df = pd.DataFrame({
+                'snp': grouped_df['snp'],
+                'x': positions_df['x'],
+                'y': positions_df['y'],
+                'p_values': grouped_df['p'],
+                'odds_ratios': grouped_df['odds_ratio'],
+                'phenotypes': grouped_df['phewas_string']
+            })
+        else:
+            results_df = pd.DataFrame({
+                'phewas_string': grouped_df['phewas_string'],
+                'x': positions_df['x'],
+                'y': positions_df['y'],
+                'p_values': grouped_df['p'],
+                'odds_ratios': grouped_df['odds_ratio'],
+                'snps': grouped_df['snp']
+            })
+        return results_df
+
+    def engineer_disease_features(self, filtered_df):
+        """
+        Helper method to engineer the features for the disease SOM.
+        :param filtered_df: Filtered DataFrame
+        :return: Features matrix and grouped DataFrame
+        """
+        # Feature engineering for diseases
+        grouped_df = filtered_df.groupby('phewas_string').agg({
+            'snp': list,
+            'gene_name': list,
+            'p': list,
+            'odds_ratio': list,
+            'category_string': 'first',
+            'l95': list,
+            'u95': list,
+            'maf': list,
+        }).reset_index()
+        # One-Hot Encode categorical variables with sparse one-hot encoding to save memory
+        all_alleles = sorted(set(allele for alleles in grouped_df['snp'] for allele in alleles))
+        ohe = OneHotEncoder(sparse_output=False)
+        # Encode the gene name variable with one-hot encoding
+        gene_name_encoded = ohe.fit_transform(
+            grouped_df['gene_name'].apply(lambda x: ','.join(set(x))).str.get_dummies(sep=',')
+        )
+        # Encode the category variable
+        category_encoded = ohe.fit_transform(grouped_df[['category_string']])
+        # Combine the encoded features
+        encoded_features = np.hstack([gene_name_encoded, category_encoded])
+
+        # Create the allele features for each row
+        def create_allele_features(df_row):
+            """
+            Function to create the allele features for each row.
+            :param df_row: The row of the DataFrame
+            :return: The combined features
+            """
+            # Create a dictionary to store the features
+            features = defaultdict(float)
+            # Iterate over the alleles, p-values, and odds ratios
+            for allele, p, or_value in zip(df_row['snp'], df_row['p'], df_row['odds_ratio']):
+                features[allele] = or_value * 5  # Scale the odds ratio by 5 for better visualisation
+            # Create the allele features
+            allele_features = [features[allele] for allele in all_alleles]
+            # Return the combined features
+            return np.hstack([allele_features, encoded_features[df_row.name]])
+
+        # Apply the function to create the allele features
+        features_matrix = grouped_df.apply(create_allele_features, axis=1)
+        return features_matrix, grouped_df
+
+    def engineer_snp_features(self, filtered_df):
+        """
+        Helper method to engineer the features for the SNP SOM.
+        :param filtered_df: Filtered DataFrame
+        :return: Features matrix and grouped DataFrame
+        """
+        # Group the data by SNP and aggregate the values
+        grouped_df = filtered_df.groupby('snp').agg({
+            'phewas_string': list,
+            'p': list,
+            'odds_ratio': list,
+            'category_string': list,
+            'l95': list,
+            'u95': list,
+            'maf': list,
+        }).reset_index()
+        # Feature engineering for SNPs
+        all_phenotypes = set([phenotype for phenotypes in grouped_df['phewas_string'] for phenotype in phenotypes])
+        exploded_df = grouped_df.explode('phewas_string').explode('category_string')
+        # One-Hot Encode categorical variables with sparse one-hot encoding to save memory
+        ohe_phenotype = OneHotEncoder(sparse_output=False)
+        ohe_category = OneHotEncoder(sparse_output=False)
+        # Encode the phenotype and category variables
+        phenotype_encoded = ohe_phenotype.fit_transform(exploded_df[['phewas_string']])
+        category_encoded = ohe_category.fit_transform(exploded_df[['category_string']])
+        # Aggregate the encoded features
+        phenotype_aggregated = pd.DataFrame(
+            phenotype_encoded,
+            columns=ohe_phenotype.categories_[0],
+            index=exploded_df['snp']
+        ).groupby('snp').sum()
+        # Aggregate the encoded features
+        category_aggregated = pd.DataFrame(
+            category_encoded,
+            columns=ohe_category.categories_[0],
+            index=exploded_df['snp']
+        ).groupby('snp').sum()
+        # Combine the aggregated features
+        encoded_features = np.hstack([phenotype_aggregated, category_aggregated])
+
+        # Create the phenotype features for each row
+        def create_phenotype_features(df_row):
+            """
+            Function to create the phenotype features for each row.
+            :param df_row: The row of the DataFrame
+            :return: The combined features
+            """
+            # Create a dictionary to store the features
+            features = defaultdict(float)
+            # Iterate over the phenotypes, p-values, and odds ratios
+            for phenotype, p, or_value in zip(df_row['phewas_string'], df_row['p'], df_row['odds_ratio']):
+                features[phenotype] = or_value * 5  # Scale the odds ratio by 5 for better visualisation
+            # Create the phenotype features
+            phenotype_features = [features[phenotype] for phenotype in all_phenotypes]
+            # Return the combined features
+            return np.hstack([phenotype_features, encoded_features[df_row.name]])
+
+        # Apply the function to create the phenotype features
+        features_matrix = grouped_df.apply(create_phenotype_features, axis=1)
+        return features_matrix, grouped_df
 
